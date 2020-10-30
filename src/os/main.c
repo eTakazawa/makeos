e503875d7dfe599a6d468b59316612acb0bfed53
eTakazawa <takazawa621@gmail.com>
2020-10-31 01:41:43 +0900

    タスク間通信の実装

--- src/os/main.c
+++ src/os/main.c
@@ -6,10 +6,14 @@
 /* システム・タスクとユーザ・スレッドの起動 */
 static int start_threads(int argc, char *argv[])
 {
-  kz_run(test10_1_main, "test10_1", 1, 0x100, 0, NULL);
+  // タスクキューに登録だけされstart_threadsに戻ってくる
+  // https://groups.google.com/g/kozos_tomonokai/c/hEJ6tSiKQJc?pli=1
+  kz_run(test11_1_main, "test11_1",  1, 0x100, 0, NULL);
+  kz_run(test11_2_main, "test11_2",  2, 0x100, 0, NULL);
   
   kz_chpri(15); /* 優先順位を下げて，アイドルスレッドに移行する */
-  INTR_ENABLE; /* 割込み有効にする */
+  INTR_ENABLE; /* 割込み有効にする */ // これは特に意味がないらしい
+  
   while (1) {
     asm volatile ("sleep"); /* 省電力モードに移行 */
   }
@@ -19,6 +23,8 @@ static int start_threads(int argc, char *argv[])
 
 int main(void)
 {
+  // 無効になるのは外部割り込み（シリアル）のみでトラップ命令は使える
+  // https://groups.google.com/g/kozos_tomonokai/c/pirZtSHElnk/m/TPKR4xegaBMJ
   INTR_DISABLE; /* 割込み無効にする */
 
   puts("kozos boot succeed!\n");
